586b609524b984ada34b49b3c820d6fd66d38e66
Charles Titus <ctitus@bnl.gov>
2024-09-13 20:13:16 -0400

    testing end of run exports

---
+++ end_of_run_export.py
@@ -0,0 +1,29 @@
+import time
+
+from prefect import flow, get_run_logger, task
+from tiled.client import from_profile
+from os.path import exists
+
+
+def initialize_tiled_client():
+    return from_profile("nsls2")
+
+@task(retries=2, retry_delay_seconds=10)
+def export_all_streams(uid, beamline_acronym="ucal"):
+    logger = get_run_logger()
+    tiled_client = initialize_tiled_client()
+    run = tiled_client[beamline_acronym]["raw"][uid]
+    proposal = run.start.get('proposal', {}).get('proposal_id', None)
+    cycle = run.start.get('cycle', None)
+    if proposal is None or cycle is None:
+        raise ValueError("Proposal Metadata not Loaded")
+    
+    export_path = f"/nsls2/data/sst/proposals/{cycle}/pass-{proposal}/"
+    logger.info(f"Generating Export for uid {run.start['uid']}")
+    logger.info(f"Export Data to {export_path}")
+    export_path_exists = exists(export_path)
+    logger.info(f"Export path exists {export_path_exists}")
+
+@flow
+def general_data_export(uid, beamline_acronym="ucal"):
+    export_all_streams(uid, beamline_acronym)
--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -1,6 +1,7 @@
 from prefect import flow, get_run_logger, task
 
 from data_validation import general_data_validation
+from end_of_run_export import general_data_export
 
 
 @task
@@ -14,4 +15,5 @@ def end_of_run_workflow(stop_doc):
     uid = stop_doc["run_start"]
     general_data_validation(uid)
     # Here is where exporters could be added
+    general_data_export(uid)
     log_completion()
